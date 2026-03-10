0eb64a14b19c049bf8bbf065a49555e0325b6fe5
Cesar Lopes <cesarlopes@agenciaphant.com.br>
2026-03-10 16:49:51 -0400

    .

--- mobile-app/index.js
+++
@@ -1,4 +0,0 @@
-import {AppRegistry} from 'react-native';
-import AppNavigator from './src/navigation/AppNavigator';
-
-AppRegistry.registerComponent('VivemusMobile', () => AppNavigator);
--- mobile-app/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ioysnjfyikrxgxvkigcp.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveXNuamZ5aWtyeGd4dmtpZ2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NzcyODYsImV4cCI6MjA4MjM1MzI4Nn0.4vlUay_KOdP04EQK9YYPvQAs99DUjBDocGNNEvy-jsk';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
